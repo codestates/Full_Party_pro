e59ab42ec51de316a09c4d6777ad050ef09b6430
Oilpalette <>
2022-01-03 12:57:34 +0900

    [SR] #37 페이지 라우팅

---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export default function Home () {
+
+  return (
+    <HomeContainer>
+      <div>홈</div>
+    </HomeContainer>
+  );
+}
---
+++ client/src/pages/Mypage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+export default function Mypage () {
+
+  return (
+    <div>
+    </div>
+  );
+}
