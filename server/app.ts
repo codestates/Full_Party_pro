7ae44b7d4b11d305d0cd36fc52f6cd07d027ff84
Jaehyun Cheong <wjdwogus786@gmail.com>
2022-01-18 16:08:31 +0900

    [TEMP] swagger 기능 정리

--- server/app.ts
+++ server/app.ts
@@ -28,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Welcome to Full Party!");
 });
@@ -46,7 +47,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: `Something went wrong: ${err}` });
 });
 
-const HTTPS_PORT = config.port || 80;
+const HTTPS_PORT = config.port || 4000;
 
 let server;
 if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
