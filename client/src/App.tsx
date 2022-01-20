8398c8e8fc7f9ef7dcd15f54d83d8b1f90170cc0
YoungHyun-Cho <psyhyun1030@gmail.com>
2022-01-21 04:08:18 +0900

    [DEV] 배포 도전

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -99,10 +99,9 @@ export default function App() {
                   <Route path=":commentId" element={<Party />} />
                 </Route>
                 <Route path="/post" element={<Post />} />
-                <Route path="/search" element={<Search />}>
-                  <Route path="/keyword/:keyword" element={<Search />} />
-                  <Route path="/tag/:tag" element={<Search />} />
-                </Route>
+                <Route path="/search" element={<Search />} />
+                  <Route path="/search/keyword/:keyword" element={<Search />} />
+                  <Route path="/search/tag/:tag" element={<Search />} />
                 <Route path="/notification" element={<Notification />} />
                 <Route path="/favorite" element={<Favorite />} />
                 <Route path="/mypage" element={<Mypage />} />
