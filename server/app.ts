ef8bde11022b119800dbfbca93fee0573a11e4b0
YoungHyun-Cho <psyhyun1030@gmail.com>
2022-01-21 22:46:48 +0900

    [DEVOPS] 서버사이드 배포 관련 app.ts 수정

--- server/app.ts
+++ server/app.ts
@@ -27,17 +27,17 @@ const corsOption = {
 };
 
 app.use(cors(corsOption));
+app.use(express.static(path.join(__dirname, "..", "client", "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // app.get('/*', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
 app.get("/*", (req: Request, res: Response) => {
-  res.status(200).sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  res.status(200).sendFile(path.join(__dirname, "..", "client", "public", "index.html"));
 });
 
 // app.get("/*", (req: Request, res: Response) => {
